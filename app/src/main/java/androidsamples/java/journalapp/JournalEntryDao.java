d5d4506a3c8a83cab139576f3e8fcaf89fda22e2
AmeyaKasture <kastureameya31@gmail.com>
2024-10-24 23:59:13 +0530

    bugged the sorting

--- app/src/main/java/androidsamples/java/journalapp/JournalEntryDao.java
+++ app/src/main/java/androidsamples/java/journalapp/JournalEntryDao.java
@@ -12,6 +12,23 @@ public interface JournalEntryDao {
     @Insert
     void insert(JournalEntry entry);
 
-    @Query("SELECT * FROM journal_table ORDER BY substr(date, instr(date, ',') + 1) DESC")
+
+    @Query("SELECT * FROM journal_table ORDER BY " +
+            "substr(date, length(date) - 3, 4) || '-' || " +
+            "(CASE " +
+            "WHEN instr(date, 'January') > 0 THEN '01' " +
+            "WHEN instr(date, 'February') > 0 THEN '02' " +
+            "WHEN instr(date, 'March') > 0 THEN '03' " +
+            "WHEN instr(date, 'April') > 0 THEN '04' " +
+            "WHEN instr(date, 'May') > 0 THEN '05' " +
+            "WHEN instr(date, 'June') > 0 THEN '06' " +
+            "WHEN instr(date, 'July') > 0 THEN '07' " +
+            "WHEN instr(date, 'August') > 0 THEN '08' " +
+            "WHEN instr(date, 'September') > 0 THEN '09' " +
+            "WHEN instr(date, 'October') > 0 THEN '10' " +
+            "WHEN instr(date, 'November') > 0 THEN '11' " +
+            "WHEN instr(date, 'December') > 0 THEN '12' " +
+            "END) || '-' || " +
+            "substr(date, instr(date, ' ') + 1, instr(date, ',') - instr(date, ' ') - 1) DESC")
     LiveData<List<JournalEntry>> getAllEntries();
 }
